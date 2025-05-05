fc350c2fc5bc3fa9bc4ff17a05a3e602afd1fe14
sonyouhy <shone0129@naver.com>
2025-05-05 16:28:16 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+  return (
+    <div>
+      <h1>Cherry Ad Platform</h1>
+      <Dashboard />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      <p>Welcome to your Ad Dashboard!</p>
+    </div>
+  )
+}
+
+export default Dashboard
