2217f559112a370c7e0414c5818d78de69d2e613
sonyouhy <shone0129@naver.com>
2025-05-05 23:16:28 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  root: '.', // 프로젝트 루트에서 index.html 찾음
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
