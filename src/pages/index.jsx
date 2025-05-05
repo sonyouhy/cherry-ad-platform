e21c5414a1495fb05604a86dc837d4feb3a7decd
sonyouhy <shone0129@naver.com>
2025-05-05 16:02:47 +0900

    Add files via upload

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white text-gray-800">
+      <h1 className="text-4xl font-bold mb-4">🍒 체리 광고 플랫폼</h1>
+      <p className="text-lg text-gray-600">귀엽고 효율적인 광고, 지금 시작하세요.</p>
+    </div>
+  );
+}
